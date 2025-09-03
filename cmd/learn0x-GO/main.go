e19194db084817bde5c16bd7f4a0b2fa8803a1e2
alisumup <ali.rizvi@sumup.com>
2025-09-03 23:57:51 +0200

    Graceful shutdown

--- cmd/learn0x-GO/main.go
+++ cmd/learn0x-GO/main.go
@@ -1,3 +1,55 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/araza95/learn0x-GO/internal/config"
+)
+
+func main() {
+	// load config
+	cfg := config.MustLoad()
+	// database config
+	// setup router
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome to GO!"))
+	})
+	// setup server
+	server := http.Server{
+		Addr:    cfg.Address,
+		Handler: router,
+	}
+
+	slog.Info("Server started.", slog.String("address", cfg.Address))
+
+	done := make(chan os.Signal, 1)
+
+	signal.Notify(done, os.Interrupt, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatal("Unable to start server")
+		}
+	}()
+	<-done
+
+	slog.Info("Shutting down...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err := server.Shutdown(ctx); err != nil {
+		slog.Error("Failed to shutdown server!", slog.String("error", err.Error()))
+	}
+
+	slog.Info("Server shutdown gracefully")
+}
